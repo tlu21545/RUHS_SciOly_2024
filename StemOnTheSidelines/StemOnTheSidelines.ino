37ca6847e8a5860c52972c27dd454a5a30050cb4
Tristan Lu <tlu21545@gmail.com>
2024-12-06 23:06:57 -0800

    Update StemOnTheSidelines.ino

--- StemOnTheSidelines/StemOnTheSidelines.ino
+++ StemOnTheSidelines/StemOnTheSidelines.ino
@@ -1,11 +1,27 @@
-#define enA 3 // right motor
-#define enB 5 // left motor
+// Library for IR Receiver Module
+#include <IRremote.h>
+
+/*
+ * Set pins for L298N motor controller.
+ * enA and enB should be PWM pins 
+ * if variable motor speed needed.
+ * in1 and in2 same value: disable motor.
+ * in1 LOW and in2 HIGH: spin one direction
+ * in1 HIGH and in2 LOW: spin opposite direction.
+ * Similar for in3 and in4.
+ */
+#define enA 3
 #define in1 6
 #define in2 7
+#define enB 5
 #define in3 8
 #define in4 9
 
-#include <IRremote.h>
+/*
+ * Commands for IR Receiver Module.
+ * Commands depend on specific remote
+ * and receiver, so test your own!
+ */
 #define IR_BUTTON_0 22
 #define IR_BUTTON_1 12
 #define IR_BUTTON_2 24
@@ -16,6 +32,8 @@
 #define IR_BUTTON_7 66
 #define IR_BUTTON_8 82
 #define IR_BUTTON_9 74
+
+// Pin for IR Receiver Module
 #define IR_RECEIVE_PIN 11
 
 void setMotors(int speed);
@@ -38,6 +56,12 @@ void setup() {
 }
 
 void loop() {
+  /*
+   * Waits for IR Receiver to receive command.
+   * Buttons 1, 2, and 9 are currently being used.
+   * Receiver stops and starts after each command
+   * to avoid reading same command twice.
+   */
   if (IrReceiver.decode()) {
     IrReceiver.resume();
     int code = IrReceiver.decodedIRData.command;
@@ -96,47 +120,71 @@ void loop() {
   }
 }
 
+/*
+ * Sets both motors to variable speed
+ * enA: right motor; enB: left motor
+ * on the 2024 robot
+ */
 void setMotors(int speed) {
   // Serial.println("setting motors");
   analogWrite(enA, speed);
   analogWrite(enB, speed);
 }
 
+/*
+ * Drive both motors in same direction for
+ * ms milliseconds at variable speed
+ */
 void driveStraight(int ms, int speed) {
   // Serial.println("starting drive straight");
   setMotors(speed);
+  // unsigned long rather than int to avoid overflow
   unsigned long referenceTime = millis();
   while (millis() - referenceTime < ms);
   resetRobot();
 }
 
+/*
+ * Drive both motors in opposite direction for
+ * ms milliseconds at variable speed. On the
+ * 2024 robot, 0: left; 1: right
+ */
 void turn(int ms, int speed, int direction) {
   // Serial.println("starting turn");
   switch (direction) {
     case 0:
-      // Serial.println("reversing left");
+      // Serial.println("reversing left motor");
       digitalWrite(in3, HIGH);
       digitalWrite(in4, LOW);
       break;
     case 1:
-      // Serial.println("reversing right");
+      // Serial.println("reversing right motor");
       digitalWrite(in1, HIGH);
       digitalWrite(in2, LOW);
       break;
   }
   
   setMotors(speed);
+  // unsigned long rather than int to avoid overflow
   unsigned long referenceTime = millis();
   while (millis() - referenceTime < ms);
   resetRobot();
 }
 
+/*
+ * Drive straight six yards at max speed
+ */
 void sixYardDash() {
   // Serial.println("started six yard dash");
   driveStraight(10000, 255);
   // Serial.println("ended six yard dash");
 }
 
+/*
+ * Complete path around four cones (specific
+ *  to 2024 competition) at max speed. Delay
+ *  between actions to allow motors to stop.
+ */
 void fourConeXDrill() {
   // Serial.println("started four cone x drill");
   driveStraight(3500, 255);
@@ -155,6 +203,10 @@ void fourConeXDrill() {
   // Serial.println("ended four cone x drill");
 }
 
+/*
+ * Reset motors back to original state
+ * (both off and in forward direction).
+ */
 void resetRobot() {
   // Serial.println("resetting robot");
   digitalWrite(in1, LOW);
